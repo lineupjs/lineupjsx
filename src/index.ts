6031e2cbf79cfd9dde34523239ebadaa621dd89f
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-12-22 13:58:37 +0100

    avoid tsx when not needed

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+export {default as LineUp, default, ILineUpProps} from './LineUp';
+export {default as Taggle} from './Taggle';
+export * from './column';
+export * from './LineUpRanking';
+export {default as LineUpRanking} from './LineUpRanking';
